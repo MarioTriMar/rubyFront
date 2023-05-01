79042a0c0187ef6c4fe8003901226c73059f1ce6
Jose Ramon Serrano López <JoseRamon.Serrano@alu.uclm.es>
2023-05-01 23:01:08 +0200

    añadir notas a coleccion

--- src/app/addnote-collection/addnote-collection.component.ts
+++ src/app/addnote-collection/addnote-collection.component.ts
@@ -21,53 +21,20 @@ export class AddnoteCollectionComponent implements OnInit {
   }
 
   loadNotes(){
-    this.noteService.getAllNotesByUserId(localStorage.getItem("idUser")!).subscribe(data=>{
-      this.notes=data
-      this.loadSharedNotes();
+    this.noteService.getAllPossibleNotes(localStorage.getItem("idUser")!, localStorage.getItem("collectionId")!).subscribe(data=>{
+      console.log(data)
+      this.notes=data;
     },error=>{
       console.log(error)
     })
-  }
-  loadCollectionNotes(){
-    this.noteService.getNotesOfCollection(localStorage.getItem("collectionId")!).subscribe(data=>{
-      this.collectionNotes=data
-      this.checkNotesOfCollection();
-    },error=>{
-      console.log(error)
-    })
-  }
-
-
-  loadSharedNotes(){
-    this.noteService.getAllSharedNotesByUserId(localStorage.getItem("idUser")!).subscribe(data=>{
-      this.sharedNotes=data
-      this.loadCollectionNotes();
-    },error=>{
-      console.log(error)
-    })
-  }
-  checkNotesOfCollection(){
-    this.allNotes=this.notes.concat(this.sharedNotes);
-    console.log(this.allNotes);
-    for (let i = 0 ; i < this.collectionNotes.length ; i++){
-      for (let j = 0 ; j < this.allNotes.length; i++){
-        if (this.collectionNotes[i]._id.$oid == this.allNotes[j]._id.$oid){
-          this.allNotes.splice(j,1);
-        }
-      }
-    }
-    console.log(this.notes);
-  }
-  //cuando hagas click en una nota se ejecuta este metodo
-  selectNote(note:Note){
-    this.selectedNote=note
   }
 
   //metodo que ejecuta el boton que estará arriba para añadir nota
   //a este boton como etiqueta se le puede poner el {{selectedNote.title}}
-  addNote(){
-    this.noteService.addNoteToCollection(localStorage.getItem("collectionId")!,this.selectedNote._id.$oid).subscribe(data=>{
+  addNote(note:Note){
+    this.noteService.addNoteToCollection(localStorage.getItem("collectionId")!,note._id.$oid).subscribe(data=>{
       console.log(data)
+      this.ngOnInit();
     },error=>{
       console.log(error)
     })
